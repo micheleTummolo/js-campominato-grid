54a36086071ebf5f7dff001c047d8f74aad24991
micheleTummolo <tummolomichele02@gmail.com>
2022-12-06 20:12:51 +0100

    Done difficulty selection

--- js/script.js
+++ js/script.js
@@ -1,24 +1,43 @@
 const playButton = document.getElementById('play_button')
 
 playButton.addEventListener ('click', function(){
+    let diff = document.getElementById('difficulty').value
+    console.log("Difficoltà: " + diff)
+    let max = 0;
     let grid = document.getElementById('grid');
+    let squareType
     
-    function createGridSquare() {
-        const element = document.createElement ('div');
-    
-        element.classList.add ('square');
-    
-        return element;
-    };
-    
-    for (let i = 0; i < 100; i++) {
+    if (diff == "easy" ){
+        max = 100
+        squareType = 'square_easy'
+    }
+
+    else if (diff == "medium"){
+        max = 81
+        squareType = 'square_medium'
+    }
+    else {
+        max = 49
+        squareType = 'square_hard'
+    }
+
+    console.log("Numero quadrati: " + max)
+
+    for (let i = 0; i < max; i++) {
         const square = createGridSquare()
         grid.appendChild(square)
-        console.log(i)
         square.addEventListener ('click', function(){
             this.classList.add ('clicked')
             console.log("Hai cliccato il quadratino numero " + (i + 1))
         })
+    };
+
+    function createGridSquare() {
+        const element = document.createElement ('div');
+    
+        element.classList.add ('square', squareType);
+    
+        return element;
     };
 })
 
